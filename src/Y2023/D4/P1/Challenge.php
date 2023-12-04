7bd607355568b05904555ecaf598fac9b0754395
Nephanae <23710017+Nephanae@users.noreply.github.com>
2023-12-04 19:17:08 +0100

    Add 2023/4/1 Challenge

---
+++ src/Y2023/D4/P1/Challenge.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Y2023\D4\P1;
+
+use App\Challenge as BaseChallenge;
+use Illuminate\Support\Collection;
+
+class Challenge extends BaseChallenge
+{
+    public function resolve(): string
+    {
+        return $this->input
+            ->map(fn($row) => explode(':', $row))
+            ->map(fn($row) => explode('|', $row[1]))
+            ->map(fn($row) => (object) [
+                'winning' => new Collection(explode(' ', trim($row[0]))),
+                'cards' => new Collection(explode(' ', trim($row[1]))),
+            ])
+            ->map(fn($row) => $row->winning->filter(fn($card) => $row->cards->contains($card))->filter()->count())
+            ->map(fn($count) => $count < 2 ? $count : pow(2, $count - 1))
+            ->sum();
+    }
+}
