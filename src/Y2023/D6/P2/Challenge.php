9af51aba276198bbe5c225796273c8d314f81ce9
Nephanae <23710017+Nephanae@users.noreply.github.com>
2023-12-06 20:18:31 +0100

    Add 2023/6/2 Challenge

---
+++ src/Y2023/D6/P2/Challenge.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Y2023\D6\P2;
+
+use App\Challenge as BaseChallenge;
+use Illuminate\Support\Collection;
+use stdClass as StdClass;
+
+class Challenge extends BaseChallenge
+{
+    public function resolve(): string
+    {
+        $race = $this->getRace();
+
+        $halfTime = $race->time / 2;
+        $time = ceil($halfTime);
+
+        while ($this->getDistance($time, $race->time) > $race->distance) {
+            $time++;
+        }
+
+        return ($time - $halfTime - 1) * 2 + ($halfTime === ceil($halfTime) ? 0 : 1);
+    }
+
+    private function getDistance(int $holdTime, int $raceTime): int
+    {
+        return $holdTime * ($raceTime - $holdTime);
+    }
+
+    private function getRace(): StdClass
+    {
+        $input = $this->input
+            ->mapWithKeys(function ($row) {
+                list($key, $values) = explode(':', $row);
+                $values = new Collection(explode(' ', $values));
+
+                return [$key => $values->filter()->implode('')];
+            })
+            ->toArray();
+
+        return (object) ['time' => $input['Time'], 'distance' => $input['Distance']];
+    }
+}
